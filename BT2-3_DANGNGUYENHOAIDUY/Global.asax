ca1f74d84e70f3e29f54d67aeec4bfdec5470048
Duy Đặng Nguyên Hoài <dangduy7102000@gmail.com>
2021-06-21 12:27:50 +0700

    first commit

---
+++ BT2-3_DANGNGUYENHOAIDUY/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BT2_3_DANGNGUYENHOAIDUY.MvcApplication" Language="C#" %>
---
+++ BT2-3_DANGNGUYENHOAIDUY/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BT2_3_DANGNGUYENHOAIDUY.Startup))]
+namespace BT2_3_DANGNGUYENHOAIDUY
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
